d7a9d20ed2aa99a4f145d04e6ecd0edd9575289f
Davronbek Abdulkhamidov <davronbekabdulkhamidov@gmail.com>
2023-02-12 11:56:54 +0500

    Cascade type

---
+++ src/test/java/uz/com/appannotationandcascade1/AppAnnotationAndCascade1ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.com.appannotationandcascade1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAnnotationAndCascade1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
